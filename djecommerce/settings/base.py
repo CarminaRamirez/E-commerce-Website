4929cda8d4f1cadb7c150561bd5503070c6af92e
CarminaRamirez <50931389+CarminaRamirez@users.noreply.github.com>
2020-07-31 11:52:22 -0300

    corregido

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -3,6 +3,7 @@ from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
+#SECRET_KEY = '!$(o+-^p)s9#%%kpx-cufza^=fu00ab!ihvkqur3dq8*z3o6us'
 
 SECRET_KEY = config('SECRET_KEY')
 
